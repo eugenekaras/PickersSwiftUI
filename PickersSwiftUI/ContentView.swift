7bc4c51016a9e2a37d8652dcbc0f1cc50f0a2391
ekaras <key.cpp@gmail.com>
2022-12-02 02:10:27 +0300

    2

--- PickersSwiftUI/ContentView.swift
+++ PickersSwiftUI/ContentView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var section = 20
+    var settingTime = ["10 min", "20 min","30 min","40 min"]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+
+        VStack{
+            
+            Picker(selection:  $section) {
+                ForEach(self.settingTime){
+                    Text(self.settingTime[$0])
+                }
+            } label: {
+                Text("Tyyyy")
+            }
+            
+            Text("Время выбрано - \( settingTime[section])")
+            
         }
-        .padding()
     }
 }
 
