c98d0d720062befffe87068f95481fc83519ceb3
ekaras <key.cpp@gmail.com>
2022-12-02 01:43:45 +0300

    1

---
+++ PickersSwiftUI/PickersSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickersSwiftUIApp.swift
+//  PickersSwiftUI
+//
+//  Created by Евгений Карась on 2.12.22.
+//
+
+import SwiftUI
+
+@main
+struct PickersSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
